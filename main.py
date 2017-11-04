35a96219a4f36d320b8aa8c893f63a2439b7b64a
EKW544 <elisawood10@aol.com>
2017-11-04 12:15:36 -0500

    Made content dynamic, random movie of the day and tomorrow's movie

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
@@ -8,6 +9,10 @@ app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 def index():
     # choose a movie by invoking our new function
     movie = get_random_movie()
+    movie2 = get_random_movie()
+
+    while movie2 == movie:
+        movie2 = get_random_movie()
 
     # build the response string
     content = "<h1>Movie of the Day</h1>"
@@ -17,13 +22,19 @@ def index():
 
     # TODO: pick another random movie, and display it under
     # the heading "<h1>Tommorrow's Movie</h1>"
-
+    content += "<h1>Tomorrow's Movie</h1>"
+    content += "<ul>"
+    content += "<li>" + movie2 + "</li>"
+    content += "</ul>"
     return content
 
 def get_random_movie():
     # TODO: make a list with at least 5 movie titles
+    movie_list = ["About Time", "Interstellar", "Up", "Ferris Bueller's Day Off", "The Matrix"]
     # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
+    movie = random.randrange(0, len(movie_list))
+    # return "The Big Lebowski"
+    return str(movie_list[movie])
 
 
 app.run()
